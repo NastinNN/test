37c9c02cfb1165adfbf1e30d136f5f282aac5de7
Анастасия <56252297+NastinNN@users.noreply.github.com>
2024-01-16 18:52:51 +0300

    Add files via upload

---
+++ Web_NNGU_Project/js/main.js
@@ -0,0 +1,59 @@
+// topSlider
+const topSlider = new Swiper('.slider', {
+    loop: true,
+    spaceBetween: 100,
+
+    autoplay: {
+      delay: 5000,
+     },
+     speed: 1500,
+
+    // If we need pagination
+    pagination: {
+      el: '.slider__nav',
+      clickable: true
+    },
+
+    on: {
+      init() {
+        this.el.addEventListener('mouseenter', () => {
+          this.autoplay.stop();
+        });
+  
+        this.el.addEventListener('mouseleave', () => {
+          this.autoplay.start();
+        });
+      }
+    },
+});
+
+// cards slider
+const swiper = new Swiper(".project__swiper", {
+  slidesPerView: 3,
+  spaceBetween: 22,
+  freeMode: true,
+
+  pagination: {
+    el: ".project__pagination",
+    clickable: true,
+  },
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
+// gallery
+const gallery = new Swiper('.gallery', {
+  slidesPerView: 3,
+  spaceBetween: 20,
+});
+
+// fancybox
+Fancybox.bind("[data-fancybox]");
